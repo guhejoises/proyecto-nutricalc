516fb15848e367d8307978bcc1bd25add6ec921e
Ethan11177 <jimimorison03@gmail.com>
2024-05-29 01:05:25 -0600

    se agregaron mas campos a la pantalla de registro de usuario

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import "./AppSass.scss";
-import { FaUser } from "react-icons/fa";
 import { MdDriveFileRenameOutline } from "react-icons/md";
 import ErrorEtiquetas from "./Componentes/ErrorEtiquetas";
 
@@ -8,6 +7,8 @@ function App() {
   const [nombre, setNombre] = useState("");
   const [apelPate, setApelPate] = useState("");
   const [apelMate, setApelMate] = useState("");
+  const [correo, setCorreo] = useState("");
+  const [error, setError] = useState('');
   let activar = 0;
 
   const cambiarNomrbe = (e) => {
@@ -24,7 +25,19 @@ function App() {
 
   const cambiarApelMate = (e) => {
     const value = e.target.value;
-    setApelPate(value);
+    setApelMate(value);
+  };
+
+  const cambiarCorreo = (e) => {
+    const value = e.target.value;
+    setCorreo(value);
+
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (correo && !emailPattern.test(correo)) {
+      setError('Correo electrónico no válido');
+    } else {
+      setError('');
+    }
   };
 
   const guardarClick = () => {
@@ -34,18 +47,138 @@ function App() {
   return (
     <div className="mi_formulario_container">
       <div className="mi_formulario_header-image">
-        <div className="mi_formulario_header">
-          <h1 className="mi_formulario_titulo">
-            Ingresar Datos del Paciente
-            <FaUser className="mi_formulario_logo" size="100px" />
-          </h1>
-        </div>
+        <h1 className="mi_formulario_titulo">Ingresar Datos del Paciente</h1>
       </div>
       <div className="mi_formulario_body">
         <div className="mi_formulario_form">
-          <div className="mi_formulario_tabla">
-            <table className="mi_formulario_tabla_contenedor">
+          <div className="table-container">
+            <table className="responsive-table">
               <tr className="mi_formulario_tabla_row">
+                <th className="mi_formulario_tabla_hurdle">
+                  <div className="mi_formulario_input-icon">
+                    <label className="mi_formulario_label">Nombre:</label>
+                    <MdDriveFileRenameOutline className="mi_formulario_icon" />
+                    <div>
+                      <input
+                        className="mi_formulario_input"
+                        id="nombre"
+                        type="text"
+                        value={nombre}
+                        onChange={cambiarNomrbe}
+                        placeholder="Ingrese el nombre"
+                        autoComplete="off"
+                        maxLength={19}
+                      ></input>
+                      <ErrorEtiquetas nombre={nombre} activar={activar} />
+                    </div>
+                  </div>
+                </th>
+
+                <th className="mi_formulario_tabla_hurdle">
+                  <div className="mi_formulario_input-icon">
+                    <label className="mi_formulario_label">
+                      Apellido Paterno:
+                    </label>
+                    <MdDriveFileRenameOutline className="mi_formulario_icon" />
+                    <input
+                      className="mi_formulario_input"
+                      id="apelPate"
+                      type="text"
+                      value={apelPate}
+                      onChange={cambiarApelPate}
+                      placeholder="Ingrese el Apellido Paterno"
+                      autoComplete="off"
+                      maxLength={19}
+                    ></input>
+                    <ErrorEtiquetas nombre={apelPate} />
+                  </div>
+                </th>
+
+                <th className="mi_formulario_tabla_hurdle">
+                  <div className="mi_formulario_input-icon">
+                    <label className="mi_formulario_label">
+                      Apellido Materno:
+                    </label>
+                    <MdDriveFileRenameOutline className="mi_formulario_icon" />
+                    <input
+                      className="mi_formulario_input"
+                      id="apelMate"
+                      type="text"
+                      value={apelMate}
+                      onChange={cambiarApelMate}
+                      placeholder="Ingrese el Apellido Materno"
+                      autoComplete="off"
+                      maxLength={19}
+                    ></input>
+                    <ErrorEtiquetas nombre={apelMate} />
+                  </div>
+                </th>
+              </tr>
+              <tr>
+                <th className="mi_formulario_tabla_hurdle">
+                  <div className="mi_formulario_input-icon">
+                    <label className="mi_formulario_label">
+                      Correo Electronico:
+                    </label>
+                    <MdDriveFileRenameOutline className="mi_formulario_icon" />
+                    <div>
+                      <input
+                        className="mi_formulario_input"
+                        id="correo_electronico"
+                        type="email"
+                        value={correo}
+                        onChange={cambiarCorreo}
+                        placeholder="ejemplo@mail.com"
+                        autoComplete="off"
+                        maxLength={25}
+                      ></input>
+                      {error && <label className="mi_formulario_error">{error}</label>}
+                    </div>
+                  </div>
+                </th>
+
+                <th className="mi_formulario_tabla_hurdle">
+                  <div className="mi_formulario_input-icon">
+                    <label className="mi_formulario_label">
+                      Número de telefono:
+                    </label>
+                    <MdDriveFileRenameOutline className="mi_formulario_icon" />
+                    <input
+                      className="mi_formulario_input"
+                      id="numero_telefono"
+                      type="text"
+                      inputMode="numeric"
+                      pattern="[0-9]*"
+                      value={apelPate}
+                      onChange={cambiarApelPate}
+                      placeholder="Ingrese el número de telefono"
+                      autoComplete="off"
+                      maxLength={10}
+                    ></input>
+                  </div>
+                </th>
+
+                <th className="mi_formulario_tabla_hurdle">
+                  <div className="mi_formulario_input-icon">
+                    <label className="mi_formulario_label">
+                      Apellido Materno:
+                    </label>
+                    <MdDriveFileRenameOutline className="mi_formulario_icon" />
+                    <input
+                      className="mi_formulario_input"
+                      id="apelMate"
+                      type="apelMate"
+                      value={apelMate}
+                      onChange={cambiarApelMate}
+                      placeholder="Ingrese el Apellido Materno"
+                      autoComplete="off"
+                      maxLength={19}
+                    ></input>
+                    <ErrorEtiquetas nombre={apelMate} />
+                  </div>
+                </th>
+              </tr>
+              <tr>
                 <th className="mi_formulario_tabla_hurdle">
                   <div className="mi_formulario_input-icon">
                     <label className="mi_formulario_label">Nombre:</label>
@@ -58,6 +191,8 @@ function App() {
                         value={nombre}
                         onChange={cambiarNomrbe}
                         placeholder="Ingrese el nombre"
+                        autoComplete="off"
+                        maxLength={19}
                       ></input>
                       <ErrorEtiquetas nombre={nombre} activar={activar} />
                     </div>
@@ -77,6 +212,8 @@ function App() {
                       value={apelPate}
                       onChange={cambiarApelPate}
                       placeholder="Ingrese el Apellido Paterno"
+                      autoComplete="off"
+                      maxLength={19}
                     ></input>
                     <ErrorEtiquetas nombre={apelPate} />
                   </div>
@@ -85,28 +222,34 @@ function App() {
                 <th className="mi_formulario_tabla_hurdle">
                   <div className="mi_formulario_input-icon">
                     <label className="mi_formulario_label">
-                      Apellido Paterno:
+                      Apellido Materno:
                     </label>
                     <MdDriveFileRenameOutline className="mi_formulario_icon" />
                     <input
                       className="mi_formulario_input"
-                      id="apelPate"
-                      type="apelPate"
-                      value={apelPate}
-                      onChange={cambiarApelPate}
+                      id="apelMate"
+                      type="apelMate"
+                      value={apelMate}
+                      onChange={cambiarApelMate}
                       placeholder="Ingrese el Apellido Materno"
+                      autoComplete="off"
+                      maxLength={19}
                     ></input>
-                    <ErrorEtiquetas nombre={apelPate} />
+                    <ErrorEtiquetas nombre={apelMate} />
                   </div>
                 </th>
               </tr>
-              <tr></tr>
             </table>
           </div>
         </div>
         <div className="mi_formulario_espacio1">
           <button
-            disabled={(apelPate.length <= 0 || nombre.length <= 0) && true}
+            disabled={
+              (apelPate.length <= 0 ||
+                apelMate.length <= 0 ||
+                nombre.length <= 0) &&
+              true
+            }
             className="mi_formulario_button"
             onClick={guardarClick}
           >
--- src/Componentes/ErrorEtiquetas.jsx
+++ src/Componentes/ErrorEtiquetas.jsx
@@ -6,22 +6,16 @@ function ErrorEtiquetas(params) {
       {(/^[a-zA-Z\s]*$/g.test(params.nombre) === false || params.activar >= 1) && (
         <label className="mi_formulario_error">
           No se aceptan caracteres numericos
-          <br />
-          <hr />
         </label>
       )}
       {(params.nombre.length <= 3 && params.activar >= 1) && (
         <label className="mi_formulario_error">
           El nombre de ser de al menos tres caracteres
-          <br />
-          <hr />
         </label>
       )}
-      {(params.nombre.length >= 15 || params.activar > 0) && (
+      {(params.nombre.length >= 20 || params.activar > 0) && (
         <label className="mi_formulario_error">
           El nombre no puede contener mas de 15 caracteres
-          <br />
-          <hr />
         </label>
       )}
     </div>

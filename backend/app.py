a21e1addc03b6500a01a16e37bb22dbd0fceba9b
guhejoises <54814411+guhejoises@users.noreply.github.com>
2024-06-08 23:18:33 -0600

    Modificacion de endpoint obtener
    Se realizo la modificacion para poder obtener los registros que coincidan en alguno o todo el tecto

--- backend/app.py
+++ backend/app.py
@@ -1,142 +1,211 @@
 from flask import Flask, request, jsonify, Response
 from flask_cors import CORS
-from product import Product
-import databasePacientes as dbase
-
-db = dbase.dbConnectionPacientes()
+from pymongo import MongoClient
+import certifi
+import traceback
+from pymongo.errors import ConnectionFailure
 
 app = Flask(__name__)
 CORS(app)  # Habilitar CORS para permitir solicitudes desde React
 
-#Guardar elemento
+# Conexión a la base de datos MongoDB
+MONGO_URI = 'mongodb+srv://jimimorison03:BjQDS0zuK3v9tKzA@cluster0.hcqwrty.mongodb.net/?retryWrites=true&w=majority'
+ca = certifi.where()
+
+
+def dbConnectionPacientes():
+    try:
+        client = MongoClient(MONGO_URI, tls=True, tlsCAFile=ca)
+        db = client["pacientes"]
+        return db
+    except ConnectionFailure as e:
+        print(f'Error de conexión con la base de datos: {e}')
+        return None
+
+
+db = dbConnectionPacientes()
+
+if db is None:
+    print("Error al conectar con la base de datos. Asegúrate de que la URI y la conexión SSL son correctas.")
+    exit(1)
+
+
+# Guardar elemento
 @app.route('/api/guardar_datos', methods=['POST'])
 def receive_data():
-    data = request.json
-    products = db['pacientes']
-    
-    # Se obtienen los datos del paciente
-    nombre = data.get("nombre")
-    apelPate = data.get("apelPate")
-    apelMate = data.get("apelMate")
-    correo = data.get("correo")
-    numero = data.get("numero")
-    gender = data.get("gender")
-    weight = data.get("weight")
-    dateOfBirth = data.get("dateOfBirth")
-    height = data.get("height")
-    circunferencia_brazo = data.get("circunferencia_brazo")
-    pliegue_triceps = data.get("pliegue_triceps")
-    actiFisica = data.get("actiFisica")
-    acidoUrico = data.get("acidoUrico")
-    albumina = data.get("albumina")
-    colesterol = data.get("colesterol")
-    globulina = data.get("globulina")
-    tension = data.get("tension")
-    hematocrito = data.get("hematocrito")
-    proteinas = data.get("proteinas")
-    triglicerido = data.get("triglicerido")
-    
-    # Verificar si ya existe un paciente con el mismo correo
-    if products.find_one({"correo": correo}):
-        return jsonify({"message": "El correo electrónico ya está registrado"}), 400
+    try:
+        data = request.json
+        products = db['pacientes']
 
-    # Inserta los datos en MongoDB
-    product = Product(nombre, apelPate, apelMate, correo, numero, gender, weight,
-                      dateOfBirth, height, circunferencia_brazo, pliegue_triceps, actiFisica, 
-                      acidoUrico, albumina, colesterol, globulina, hematocrito, proteinas, 
-                      tension, triglicerido)
-    
-    result = products.insert_one(product.toDBCollection())
+        # Se obtienen los datos del paciente
+        nombre = data.get("nombre")
+        apelPate = data.get("apelPate")
+        apelMate = data.get("apelMate")
+        correo = data.get("correo")
+        numero = data.get("numero")
+        gender = data.get("gender")
+        weight = data.get("weight")
+        dateOfBirth = data.get("dateOfBirth")
+        height = data.get("height")
+        circunferencia_brazo = data.get("circunferencia_brazo")
+        pliegue_triceps = data.get("pliegue_triceps")
+        actiFisica = data.get("actiFisica")
+        acidoUrico = data.get("acidoUrico")
+        albumina = data.get("albumina")
+        colesterol = data.get("colesterol")
+        globulina = data.get("globulina")
+        tension = data.get("tension")
+        hematocrito = data.get("hematocrito")
+        proteinas = data.get("proteinas")
+        triglicerido = data.get("triglicerido")
 
-    # Verificar si la inserción fue exitosa
-    if result.inserted_id:
-        return jsonify({"message": "Datos insertados con éxito", "inserted_id": str(result.inserted_id)}), 200
-    else:
-        return jsonify({"message": "Error al insertar los datos"}), 500
+        # Verificar si ya existe un paciente con el mismo correo
+        if products.find_one({"correo": correo}):
+            return jsonify({"message": "El correo electrónico ya está registrado"}), 400
 
-#Eliminar elemento
+        # Inserta los datos en MongoDB
+        product = {
+            "nombre": nombre,
+            "apelPate": apelPate,
+            "apelMate": apelMate,
+            "correo": correo,
+            "numero": numero,
+            "gender": gender,
+            "weight": weight,
+            "dateOfBirth": dateOfBirth,
+            "height": height,
+            "circunferencia_brazo": circunferencia_brazo,
+            "pliegue_triceps": pliegue_triceps,
+            "actiFisica": actiFisica,
+            "acidoUrico": acidoUrico,
+            "albumina": albumina,
+            "colesterol": colesterol,
+            "globulina": globulina,
+            "tension": tension,
+            "hematocrito": hematocrito,
+            "proteinas": proteinas,
+            "triglicerido": triglicerido
+        }
+
+        result = products.insert_one(product)
+
+        # Verificar si la inserción fue exitosa
+        if result.inserted_id:
+            return jsonify({"message": "Datos insertados con éxito", "inserted_id": str(result.inserted_id)}), 200
+        else:
+            return jsonify({"message": "Error al insertar los datos"}), 500
+    except Exception as e:
+        traceback.print_exc()
+        return jsonify({"message": "Error interno del servidor", "error": str(e)}), 500
+
+
+# Eliminar elemento
 @app.route('/delete/<string:correo>', methods=['DELETE'])
 def delete(correo):
-    paciente = db['pacientes']
-    result = paciente.delete_one({'correo': correo})
+    try:
+        paciente = db['pacientes']
+        result = paciente.delete_one({'correo': correo})
 
-    if result.deleted_count == 1:
-        return jsonify({"message": "Paciente eliminado con éxito"}), 200
-    else:
-        return jsonify({"message": "Error al eliminar el paciente"}), 404
+        if result.deleted_count == 1:
+            return jsonify({"message": "Paciente eliminado con éxito"}), 200
+        else:
+            return jsonify({"message": "Error al eliminar el paciente"}), 404
+    except Exception as e:
+        traceback.print_exc()
+        return jsonify({"message": "Error interno del servidor", "error": str(e)}), 500
 
-#Editar elemento
+
+# Editar elemento
 @app.route('/edit/<string:correo_edit>', methods=['POST'])
 def edit(correo_edit):
-    data = request.json
-    products = db['pacientes']
-    
-    # Se obtienen los datos del paciente
-    update_fields = {
-        "nombre": data.get("nombre"),
-        "apelPate": data.get("apelPate"),
-        "apelMate": data.get("apelMate"),
-        "numero": data.get("numero"),
-        "gender": data.get("gender"),
-        "weight": data.get("weight"),
-        "dateOfBirth": data.get("dateOfBirth"),
-        "height": data.get("height"),
-        "actiFisica": data.get("actiFisica"),
-        "datosAntopometricos" : {
-            "circunferencia_brazo": data.get("circunferencia_brazo"),
-            "pliegue_triceps": data.get("pliegue_triceps"),
-        },
-        "datosBiometricos" : {
-            "acidoUrico": data.get("acidoUrico"),
-            "albumina": data.get("albumina"),
-            "colesterol": data.get("colesterol"),
-            "globulina": data.get("globulina"),
-            "tension": data.get("tension"),
-            "hematocrito": data.get("hematocrito"),
-            "proteinas": data.get("proteinas"),
-            "triglicerido": data.get("triglicerido")
+    try:
+        data = request.json
+        products = db['pacientes']
+
+        # Se obtienen los datos del paciente
+        update_fields = {
+            "nombre": data.get("nombre"),
+            "apelPate": data.get("apelPate"),
+            "apelMate": data.get("apelMate"),
+            "numero": data.get("numero"),
+            "gender": data.get("gender"),
+            "weight": data.get("weight"),
+            "dateOfBirth": data.get("dateOfBirth"),
+            "height": data.get("height"),
+            "actiFisica": data.get("actiFisica"),
+            "datosAntopometricos": {
+                "circunferencia_brazo": data.get("circunferencia_brazo"),
+                "pliegue_triceps": data.get("pliegue_triceps"),
+            },
+            "datosBiometricos": {
+                "acidoUrico": data.get("acidoUrico"),
+                "albumina": data.get("albumina"),
+                "colesterol": data.get("colesterol"),
+                "globulina": data.get("globulina"),
+                "tension": data.get("tension"),
+                "hematocrito": data.get("hematocrito"),
+                "proteinas": data.get("proteinas"),
+                "triglicerido": data.get("triglicerido")
+            }
         }
-    }
 
-    result = products.update_one({'correo': correo_edit}, {'$set': update_fields})
+        result = products.update_one({'correo': correo_edit}, {'$set': update_fields})
 
-    if result.modified_count == 1:
-        return jsonify({"message": "Datos actualizados con éxito"}), 200
-    else:
-        return jsonify({"message": "Error al actualizar los datos o no se encontraron cambios"}), 404
+        if result.modified_count == 1:
+            return jsonify({"message": "Datos actualizados con éxito"}), 200
+        else:
+            return jsonify({"message": "Error al actualizar los datos o no se encontraron cambios"}), 404
+    except Exception as e:
+        traceback.print_exc()
+        return jsonify({"message": "Error interno del servidor", "error": str(e)}), 500
 
-#Obtener elemento
-@app.route('/obtener/<string:correo_obtener>', methods=['GET'])
-def obtener(correo_obtener):
-    products = db['pacientes']
-    
-    # Buscar el paciente por correo
-    paciente = products.find_one({"correo": correo_obtener})
-    
-    if paciente:
-        # Convertir ObjectId a string
-        paciente['_id'] = str(paciente['_id'])
-        return jsonify(paciente), 200
-    else:
-        return jsonify({"message": "Paciente no encontrado"}), 404
-    
+
+# Obtener elemento
+@app.route('/obtener', methods=['GET'])
+def obtener():
+    try:
+        products = db['pacientes']
+        search_query = request.args.get('query', '')
+
+        if search_query:
+            # Buscar pacientes cuyo correo contenga la palabra de búsqueda
+            pacientes = products.find({"correo": {"$regex": search_query, "$options": "i"}})
+        else:
+            # Obtener todos los pacientes
+            pacientes = products.find()
+
+        pacientes_list = []
+        for paciente in pacientes:
+            paciente['_id'] = str(paciente['_id'])  # Convertir ObjectId a string
+            pacientes_list.append(paciente)
+
+        return jsonify(pacientes_list), 200
+    except Exception as e:
+        traceback.print_exc()
+        return jsonify({"message": "Error interno del servidor", "error": str(e)}), 500
+
+
+
 # Obtener elementos
 @app.route('/obtener_elementos', methods=['GET'])
 def obtener_elementos():
-    products = db['pacientes']
-    
-    # Obtener todos los documentos de la colección
-    pacientes = products.find()
-    
-    # Convertir los documentos a una lista de diccionarios
-    pacientes_list = []
-    for paciente in pacientes:
-        paciente['_id'] = str(paciente['_id'])  # Convertir ObjectId a string
-        pacientes_list.append(paciente)
-    
-    return jsonify(pacientes_list), 200
+    try:
+        products = db['pacientes']
 
+        # Obtener todos los documentos de la colección
+        pacientes = products.find()
+
+        # Convertir los documentos a una lista de diccionarios
+        pacientes_list = []
+        for paciente in pacientes:
+            paciente['_id'] = str(paciente['_id'])  # Convertir ObjectId a string
+            pacientes_list.append(paciente)
+
+        return jsonify(pacientes_list), 200
+    except Exception as e:
+        traceback.print_exc()
+        return jsonify({"message": "Error interno del servidor", "error": str(e)}), 500
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
